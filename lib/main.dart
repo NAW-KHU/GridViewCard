ab71baa249b357a356d667d34085af9d3b3a6f51
NAW-KHU <khusaywah2315@gmail.com>
2024-01-24 13:50:23 +0630

    initialize

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String>images = [
+      'assets/images/apples.jpeg',
+      'assets/images/bananas.jpeg',
+      'assets/images/cherries.jpeg',
+      'assets/images/grapes.png',
+      'assets/images/oranges.jpeg',
+      'assets/images/peaches.jpeg',
+      'assets/images/Plum.jpg',
+      'assets/images/rasberries.jpeg',
+      'assets/images/strawberries.jpeg',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('GridView'),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Center(
+          child:GridView.builder(
+                  itemCount: images.length,//to fix RangeError(index)
+                  gridDelegate:
+                      const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    crossAxisSpacing: 5.0,
+                    mainAxisSpacing: 5.0,
+                  ),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.asset(images[index]);
+                  })             
+        ),
+      ),
+    );
+  }
+}
