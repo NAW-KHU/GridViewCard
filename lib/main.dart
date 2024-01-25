4446edb4197fbcdc3a0615985d94bbe09943e1f3
NAW-KHU <khusaywah2315@gmail.com>
2024-01-25 16:06:26 +0630

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,84 +4,119 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // @override
+//   Widget build(BuildContext context) {
+//     // String src =
+//     //     'https://cdn.pixabay.com/photo/2021/06/01/07/03/sparrow-6300790_960_720.jpg';
+//     String title = 'Apples';
+// //https://pixabay.com/users/schauhi-2509795/
+
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: Scaffold(
+//       appBar: AppBar(
+//         leading: const Icon(Icons.menu),
+//         title: const Text('Cards List',
+//         style: TextStyle(
+//           color: Colors.white,
+//           fontWeight: FontWeight.bold,
+//         )),
+//         backgroundColor: Colors.blue[300],
+//       ),
+//       body: ListView(
+//         children: <Widget>[
+//           Card(
+//             child: Column(
+//               children: [
+//                 // Image.network(src),
+//                 Image.asset('assets/images/apples.jpeg'),
+//                 Text(title,
+//                 style: const TextStyle(
+//                   fontSize: 20.0,
+//                   fontWeight: FontWeight.bold,
+//                   color: Colors.blue,
+//                  ),),
+//               ],
+//             ),
+//           ),
+//         ],
+//       )
+//     ));
+//   }
+
+  List<String> src = [
+    'assets/images/apples.jpeg',
+    'assets/images/bananas.jpeg',
+    'assets/images/cherries.jpeg',
+    'assets/images/grapes.png',
+    'assets/images/oranges.jpeg',
+    'assets/images/peaches.jpeg',
+    'assets/images/Plum.jpg',
+    'assets/images/rasberries.jpeg',
+    'assets/images/strawberries.jpeg',
+  ];
+  List<String> title = [
+    "Apples",
+    "Bananas",
+    "Cherries",
+    "Grapes",
+    "Oranges",
+    "Peaches",
+    "Plum",
+    "Rasberries",
+    "Strawberries"
+  ];
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        
         appBar: AppBar(
-          leading: const Icon(Icons.menu),
-          title: const Text(
-            'Friend List',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              fontFamily: 'Times New Roman',
-            ),
-          ),
-          backgroundColor: Colors.green[500],
+          leading: const Text('Card List'),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            // Expanded(
-              Card(
-                color: Colors.yellow[50],
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  textDirection: null,
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(100),
-                      child: Image.asset(
-                        'assets/images/cherries.jpeg',
-                        height: 90,
-                        width: 90,
-                        scale: 1.5,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    Container(
-                      // width: 100,
-                      height: 100,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8.0)),
-                    ),
-                    const Column(
-                      // mainAxisSize: MainAxisSize.max,
-                      // mainAxisAlignment: MainAxisAlignment.start,
-                      textDirection: TextDirection.ltr,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hello!',
-                          textAlign: TextAlign.left,
-                          // maxLines: 2,
-                          style: TextStyle(
-                            fontSize: 15.0,
-                          ),
-                        ),
-                        Text(
-                          'I am Cherry, Wanna be your friend!',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            fontSize: 15.0,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
-
-            // ),
-         ],
-        ),
+        body: ListView.builder(
+          itemCount: src.length,
+          itemBuilder: (BuildContext context, int index)
+          
+          {
+            return card(src[index], title[index], context);
+          }),
       ),
     );
+  }
+
+  Widget card(String src, String title, BuildContext context) {
+    return Card(
+        color: Colors.yellow[50],
+        elevation: 8.0,
+        margin: const EdgeInsets.all(4.0),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Image.asset(
+              src,
+              height: MediaQuery.of(context).size.width * (3 / 4),
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Text(
+            title,
+            // ignore: prefer_const_constructors
+            style: TextStyle(
+              fontSize: 38.0,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+        ])
+       
+        );
   }
 }
