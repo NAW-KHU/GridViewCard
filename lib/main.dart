78fe0adeec5eff3feabd98cd70ac3411ea824a2c
NAW-KHU <khusaywah2315@gmail.com>
2024-02-01 16:23:17 +0630

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,129 +4,70 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+class MyApp extends StatelessWidget {
+  // const MyApp({Key? key, required this.title}):super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
-List<String> image = [
-  'assets/images/man1.jpeg',
-  'assets/images/man2.jpeg',
-  'assets/images/woman1.jpeg',
-  'assets/images/man3.jpg',
-  'assets/images/woman2.jpeg',
-  'assets/images/woman3.png',
-  'assets/images/woman3.png',
-  'assets/images/woman3.png',
-];
-final ScrollController _scrollController = ScrollController();
-
-// List<String> greeting = ['Hello'];
-List<String> name = [
-  'Jimmy',
-  'Kelvin',
-  'Delia',
-  'Franklin',
-  'Rosy',
-  'Anna',
-  'Debora',
-  'Clara'
-];
-
-class _MyAppState extends State<MyApp> {
+  // final String title;
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          leading: const Icon(Icons.people),
-          backgroundColor: Colors.green[500],
-          title: const Text(
-            "Friend List",
-            style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.normal,
-                color: Colors.black),
-          ),
-        ),
-        body: Scrollbar(
-          //Assign the scrollController to the controller property
-          controller: _scrollController,
-          // specify scrollbar properties such as thickness
-           thumbVisibility: true,// Ensure the scrollbar is always visible
-           thickness: 8.0,// Set the thickness of the scrollbar
-          //Wrap the ListView.builder with a NotificationListener to handle scroll events
-          child: NotificationListener<ScrollNotification>(
-            onNotification: (ScrollNotification notification) {
-              if (notification is ScrollUpdateNotification) {
-                _scrollController.jumpTo(notification.metrics.pixels);
-              }
-              return false;
-            },
-          child: ListView.builder(
-              controller: _scrollController,
-              itemCount: image.length,
-              itemBuilder: (BuildContext context, int index) {
-                return card(image[index], name[index], context);
-              }),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget card(String image, String name, BuildContext context) {
-    return Card(
-      color: Colors.yellow[50],
-      child: Expanded(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          textDirection: null,
-          children: <Widget>[
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100),
-              child: Image.asset(
-                image,
-                height: 100,
-                width: 100,
-                scale: 1.5,
-                fit: BoxFit.fill,
-              ),
-            ),
-            Container(
-              // width: 100,
-              height: 100,
-              decoration:
-                  BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-            ),
-            Column(
-              // mainAxisSize: MainAxisSize.max,
-              // mainAxisAlignment: MainAxisAlignment.start,
-              textDirection: TextDirection.ltr,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(
-                  name,
-                  textAlign: TextAlign.left,
-                  // maxLines: 2,
-                  style: const TextStyle(
-                    fontSize: 15.0,
-                  ),
-                ),
-                const Text(
-                  'I am your friend',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 15.0,
-                  ),
-                ),
+    return WidgetsApp(
+        debugShowCheckedModeBanner: false,
+        color: Colors.white,
+        builder: (context, child) {
+          return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              localizationsDelegates: const [
+                DefaultMaterialLocalizations.delegate,
+                DefaultWidgetsLocalizations.delegate,
               ],
-            ),
-          ],
-        ),
-      ),
-    );
+              supportedLocales: const [Locale('en', 'US')],
+              home: Scaffold(
+                backgroundColor: Colors.amber[50],
+                appBar: AppBar(
+                  backgroundColor: Colors.red,
+                  title: const Text("All About Flutter"),
+                ),
+                body: Center(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              //do not use const at AlertDialog()
+                              title: const Text('Alert Dialog Tutorial'),
+                              content:
+                                  const Text('Welcome to AllAboutflutter.'),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.pop(
+                                        context); //Invalid const will be prompt here
+                                    // child: const Text('Close'),
+                                    
+                                  },
+                                  child: const Text('Close'),
+                                ),
+                              ],
+                            );
+                          }
+                          );
+                    },
+                    style: const ButtonStyle(
+                        padding: MaterialStatePropertyAll(
+                          EdgeInsets.all(15.0),
+                        ),
+                        backgroundColor: MaterialStatePropertyAll(Colors.red)),
+                    child: const Text(
+                      'Show Alert Dialog',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ));
+        });
   }
+  // return MaterialApp(
+  //   debugShowCheckedModeBanner: false,
 }
