2d3a4d815116ee4f5486e9e2c3b678e8f13f975b
NAW-KHU <khusaywah2315@gmail.com>
2024-02-01 10:13:01 +0630

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -85,7 +85,6 @@ void main() {
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -99,10 +98,20 @@ List<String> image = [
   'assets/images/woman3.png',
   'assets/images/woman3.png',
   'assets/images/woman3.png',
-]; 
+];
+final ScrollController _scrollController = ScrollController();
 
 // List<String> greeting = ['Hello'];
-List<String> name = ['Jimmy', 'Kelvin', 'Delia', 'Franklin', 'Rosy', 'Anna', 'Debora', 'Clara'];
+List<String> name = [
+  'Jimmy',
+  'Kelvin',
+  'Delia',
+  'Franklin',
+  'Rosy',
+  'Anna',
+  'Debora',
+  'Clara'
+];
 
 class _MyAppState extends State<MyApp> {
   @override
@@ -110,22 +119,40 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            leading: const Icon(Icons.people),
-            backgroundColor: Colors.green[500],
-            title: const Text(
-              "Friend List",
-              style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.black),
-            ),
+        appBar: AppBar(
+          leading: const Icon(Icons.people),
+          backgroundColor: Colors.green[500],
+          title: const Text(
+            "Friend List",
+            style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.normal,
+                color: Colors.black),
           ),
-          body: ListView.builder(
+        ),
+        body: Scrollbar(
+          //Assign the scrollController to the controller property
+          controller: _scrollController,
+          // specify scrollbar properties such as thickness
+           thumbVisibility: true,// Ensure the scrollbar is always visible
+           thickness: 8.0,// Set the thickness of the scrollbar
+          //Wrap the ListView.builder with a NotificationListener to handle scroll events
+          child: NotificationListener<ScrollNotification>(
+            onNotification: (ScrollNotification notification) {
+              if (notification is ScrollUpdateNotification) {
+                _scrollController.jumpTo(notification.metrics.pixels);
+              }
+              return false;
+            },
+          child: ListView.builder(
+              controller: _scrollController,
               itemCount: image.length,
               itemBuilder: (BuildContext context, int index) {
                 return card(image[index], name[index], context);
-              })),
+              }),
+          ),
+        ),
+      ),
     );
   }
 
@@ -133,57 +160,52 @@ class _MyAppState extends State<MyApp> {
     return Card(
       color: Colors.yellow[50],
       child: Expanded(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.start,
-        textDirection: null,
-        children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.circular(100),
-            child: Image.asset(
-              image,
-              height: 100,
-              width: 100,
-              scale: 1.5,
-              fit: BoxFit.fill,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          textDirection: null,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset(
+                image,
+                height: 100,
+                width: 100,
+                scale: 1.5,
+                fit: BoxFit.fill,
+              ),
             ),
-          ),
-          Container(
-            // width: 100,
-            height: 100,
-            decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
-          ),
-          Column(
-            // mainAxisSize: MainAxisSize.max,
-            // mainAxisAlignment: MainAxisAlignment.start,
-            textDirection: TextDirection.ltr,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                name,
-                textAlign: TextAlign.left,
-                // maxLines: 2,
-                style: const TextStyle(
-                  fontSize: 15.0,
+            Container(
+              // width: 100,
+              height: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(8.0)),
+            ),
+            Column(
+              // mainAxisSize: MainAxisSize.max,
+              // mainAxisAlignment: MainAxisAlignment.start,
+              textDirection: TextDirection.ltr,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  name,
+                  textAlign: TextAlign.left,
+                  // maxLines: 2,
+                  style: const TextStyle(
+                    fontSize: 15.0,
+                  ),
                 ),
-              ),
-              const Text(
-                'I am your friend',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  fontSize: 15.0,
+                const Text(
+                  'I am your friend',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    fontSize: 15.0,
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
-      
-      
-      
-      )
-      
     );
   }
 }
-
-
