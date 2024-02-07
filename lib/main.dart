43d30963ffb51616c589fb95c38f88c7840a5b0e
NAW-KHU <khusaywah2315@gmail.com>
2024-02-07 10:56:04 +0630

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,2 @@
-//Navigation.pop()
 
-import 'package:flutter/material.dart';
-import 'package:grid_view_card/firstScreen.dart';
-import 'package:grid_view_card/secondScreen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    initialRoute: '/firstScreen', //root page
-    routes: {
-      '/firstScreen': (context) => const FirstScreen(),
-      '/secondScreen': (context) => const SecondScreen(),
-    },
-  ));
-}
